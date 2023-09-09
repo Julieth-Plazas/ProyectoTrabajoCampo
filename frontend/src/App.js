bd2ce9e403f16a74285a7780125fc288c6aa8444
natalia mogollón <110124286+nathis06@users.noreply.github.com>
2023-09-08 21:05:38 -0500

    Courses Games

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import Layout from './layout/Layout'
-import ParvulosGames from './components/ParvulosGames'
 
 const App = () => {
   return (
-    //<NextUIProvider>
-    //<Layout/>
+    <NextUIProvider>
+    <Layout/>
 
-    // </NextUIProvider>
-    <ParvulosGames />
+    </NextUIProvider>
   )
 }
 
