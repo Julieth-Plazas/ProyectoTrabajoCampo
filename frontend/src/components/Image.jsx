66ac528974ab07b2458129494a56f72c4941baa0
Julieth-Plazas <juliethpr9@gmail.com>
2023-09-22 10:26:44 -0500

    carrusel

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
-import '../assets/styles/post.css'
+import React, { useState } from 'react';
+import '../assets/styles/post.css';
+
 const Image = ({ post }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    return (
-        <div class="p-2">
-            <div class="">
-                <div class="relative group">
-                    <img
-                        src={post.rutaImagen}
-                        alt={post.titulo} class="w-full rounded shadow-xl hover:shadow-2xl" />
-                    <div
-                        class="flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full">
-                    </div>
-                    <div class="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
-                        <div class="flex-row text-center">
-                            <h1 class="text-gray-50 font-bold text-lg">{post.titulo}</h1>
-                            <p class="text-gray-200 font-medium text-sm"></p>
-                            <small class="text-xs font-light text-gray-300">{post.descripcion}</small>
-                        </div>
-                    </div>
-                </div>
+  const nextImage = () => {
+    const nextIndex = (currentImageIndex + 1) % post.imagenes.length;
+    setCurrentImageIndex(nextIndex);
+  };
+
+  const prevImage = () => {
+    const prevIndex = (currentImageIndex - 1 + post.imagenes.length) % post.imagenes.length;
+    setCurrentImageIndex(prevIndex);
+  };
+
+  return (
+    <div className="p-2">
+      <div className="">
+        <div className="relative group">
+          <img
+            src={post.imagenes[currentImageIndex]}
+            alt={post.titulo}
+            className="w-72 h-72 rounded-lg shadow-xl hover:shadow-2xl" 
+          />
+          <div className="flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
+          <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
+            <div className="flex-row text-center">
+              <h1 className="text-gray-50 font-bold text-lg">{post.titulo}</h1>
+              <p className="text-gray-200 font-medium text-sm"></p>
+              <small className="text-xs font-light text-gray-300">{post.descripcion}</small>
+              <div className="flex justify-between mt-2">
+                <button
+                  onClick={prevImage}
+                  className="text-gray-200 font-medium text-sm cursor-pointer"
+                >
+                  &#8592; 
+                </button>
+                <button
+                  onClick={nextImage}
+                  className="text-gray-200 font-medium text-sm cursor-pointer"
+                >
+                   &#8594;
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Image
+export default Image;
